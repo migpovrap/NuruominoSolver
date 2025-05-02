4b58f7ca651a23d132cb0e3ea91363018c69abb1
Miguel Raposo <migpovrap@hotmail.com>
2025-05-02 08:51:38 +0100

    feat(base_code): added teacher base code + enunciado + public tests

---
+++ src/nuruomino.py
@@ -0,0 +1,89 @@
+# nuruomino.py: Template para implementação do projeto de Inteligência Artificial 2024/2025.
+# Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
+# Além das funções e classes sugeridas, podem acrescentar outras que considerem pertinentes.
+
+# Grupo 00:
+# 00000 Nome1
+# 00000 Nome2
+
+class NuruominoState:
+    state_id = 0
+
+    def __init__(self, board):
+        self.board = board
+        self.id = Nuroumino.state_id
+        Nuroumino.state_id += 1
+
+    def __lt__(self, other):
+        """ Este método é utilizado em caso de empate na gestão da lista
+        de abertos nas procuras informadas. """
+        return self.id < other.id
+
+class Board:
+    """Representação interna de um tabuleiro do Puzzle Nuruomino."""
+
+    def adjacent_regions(self, region:int) -> list:
+        """Devolve uma lista das regiões que fazem fronteira com a região enviada no argumento."""
+        #TODO
+        pass
+    
+    def adjacent_positions(self, row:int, col:int) -> list:
+        """Devolve as posições adjacentes à região, em todas as direções, incluindo diagonais."""
+        #TODO
+        pass
+
+    def adjacent_values(self, row:int, col:int) -> list:
+        """Devolve os valores das celulas adjacentes à região, em todas as direções, incluindo diagonais."""
+        #TODO
+        pass
+    
+    
+    @staticmethod
+    def parse_instance():
+        """Lê o test do standard input (stdin) que é passado como argumento
+        e retorna uma instância da classe Board.
+
+        Por exemplo:
+            $ python3 pipe.py < test-01.txt
+
+            > from sys import stdin
+            > line = stdin.readline().split()
+        """
+        #TODO
+        pass    
+
+    # TODO: outros metodos da classe Board
+
+class Nuruomino(Problem):
+    def __init__(self, board: Board):
+        """O construtor especifica o estado inicial."""
+        #TODO
+        pass 
+
+    def actions(self, state: NuruominoState):
+        """Retorna uma lista de ações que podem ser executadas a
+        partir do estado passado como argumento."""
+        #TODO
+        pass 
+
+    def result(self, state: NuruominoState, action):
+        """Retorna o estado resultante de executar a 'action' sobre
+        'state' passado como argumento. A ação a executar deve ser uma
+        das presentes na lista obtida pela execução de
+        self.actions(state)."""
+
+        #TODO
+        pass 
+        
+
+    def goal_test(self, state: NuruominoState):
+        """Retorna True se e só se o estado passado como argumento é
+        um estado objetivo. Deve verificar se todas as posições do tabuleiro
+        estão preenchidas de acordo com as regras do problema."""
+        #TODO
+        pass 
+
+    def h(self, node: Node):
+        """Função heuristica utilizada para a procura A*."""
+        # TODO
+        pass
